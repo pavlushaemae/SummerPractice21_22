2e9d00da9977bb4e50594a0be82a5cef5b931c91
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-07-01 18:58:05 +0300

    update homework 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,34 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
-    private var appeal: TextView? = null
-    private var result: TextView? = null
-    private var button: Button? = null
-    private var weight: EditText? = null
-    private var height: EditText? = null
-    private var age: EditText? = null
-    private var name: EditText? = null
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        appeal = findViewById(R.id.tv_please)
-        button = findViewById(R.id.btn_chponk)
-        weight = findViewById(R.id.et_weight)
-        height = findViewById(R.id.et_height)
-        age = findViewById(R.id.et_age)
-        name = findViewById(R.id.et_name)
-        result = findViewById(R.id.te_result)
+        val button: Button? = findViewById(R.id.btn_chponk)
+        val weight: TextInputLayout? = findViewById(R.id.et_weight)
+        val height: TextInputLayout? = findViewById(R.id.et_height)
+        val age: TextInputLayout? = findViewById(R.id.et_age)
+        val name: TextInputLayout? = findViewById(R.id.et_name)
+        val result: TextView? = findViewById(R.id.te_result)
         button?.setOnClickListener {
-            try{
-                val weightNumber = weight?.text.toString().toFloat()
-                val heightNumber = height?.text.toString().toInt()
-                val ageNumber = age?.text.toString().toInt()
-                val lengthOfName = name?.text.toString().count()
+            try {
+                val weightNumber = weight?.editText.toString().toFloat()
+                val heightNumber = height?.editText.toString().toInt()
+                val ageNumber = age?.editText.toString().toInt()
+                val lengthOfName = name?.editText.toString().count()
 
                 if (ageNumber in 1..149 &&
                     lengthOfName in 1..50 &&
@@ -39,18 +30,17 @@ class MainActivity : AppCompatActivity() {
                     weightNumber > 0 &&
                     weightNumber < 250
                 ) {
-                    val value = (weightNumber + heightNumber + ageNumber + lengthOfName)*1000
+                    val value = (weightNumber + heightNumber + ageNumber + lengthOfName) * 1000
                     result?.text = "Вы любите кошек на: $value%. Поздравляю!"
-                    Log.e("MainActivity","Вы любите кошек на: $value%")
+                    Log.e("MainActivity", "Вы любите кошек на: $value%")
                 } else {
-                    result?.text = "Данные введены некорректно."
-                    Log.e("MainActivity","Данные введены некорректно.")
+                    result?.text = getString(R.string.incorrect)
+                    Log.e("MainActivity", getString(R.string.incorrect))
                 }
-            }catch (e: Exception){
-                result?.text = "Данные введены некорректно."
-                Log.e("MainActivity","Данные введены некорректно.")
+            } catch (e: Exception) {
+                result?.text = getString(R.string.incorrect)
+                Log.e("MainActivity", getString(R.string.incorrect))
             }
         }
-
     }
 }
