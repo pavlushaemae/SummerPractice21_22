db5538f1a994f33d04eee888a4681b4222707651
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-07-01 19:53:04 +0300

    update homework 1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -12,17 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button: Button? = findViewById(R.id.btn_chponk)
-        val weight: TextInputLayout? = findViewById(R.id.et_weight)
-        val height: TextInputLayout? = findViewById(R.id.et_height)
-        val age: TextInputLayout? = findViewById(R.id.et_age)
-        val name: TextInputLayout? = findViewById(R.id.et_name)
+        val weight: TextInputEditText? = findViewById(R.id.et_weight)
+        val height: TextInputEditText? = findViewById(R.id.et_height)
+        val age: TextInputEditText? = findViewById(R.id.et_age)
+        val name: TextInputEditText? = findViewById(R.id.et_name)
         val result: TextView? = findViewById(R.id.te_result)
         button?.setOnClickListener {
             try {
-                val weightNumber = weight?.editText.toString().toFloat()
-                val heightNumber = height?.editText.toString().toInt()
-                val ageNumber = age?.editText.toString().toInt()
-                val lengthOfName = name?.editText.toString().count()
+                val weightNumber = (weight?.text ?: "ERROR").toString().toFloat()
+                val heightNumber = (height?.text ?: "ERROR").toString().toInt()
+                val ageNumber = (age?.text ?: "ERROR").toString().toInt()
+                val lengthOfName = (name?.text ?: "ERROR").toString().count()
 
                 if (ageNumber in 1..149 &&
                     lengthOfName in 1..50 &&
