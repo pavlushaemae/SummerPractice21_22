5ef87c2307d4656560048766eb2e6a91fe8ff87f
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-07-03 03:16:19 +0300

    hw3 updated 03.07 (2)

--- app/src/main/java/com/example/myapplication/SecondaryFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondaryFragment.kt
@@ -6,27 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.FragmentSecondaryBinding
+import com.example.myapplication.recycler.PlanetRepository.planets
 
-class SecondaryFragment: Fragment() {
+class SecondaryFragment : Fragment() {
     private var _binding: FragmentSecondaryBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_secondary, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentSecondaryBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
-//        val args: SecondaryFragmentArgs by navArgs()
-//        val text = args.myArg
-//        binding.tvName.text = "$text"
         val args by navArgs<SecondaryFragmentArgs>()
-        val text = args.myArgg
-        binding.tvName.text = "$text"
+        val text = args.myArg
+        binding.tvName.text = "Name: ${planets[text].name}"
+        binding.tvDescription.text = "Description: ${planets[text].description}"
+        Glide.with(this).load(planets[text].url).into(binding.ivPicture)
     }
 
     override fun onDestroyView() {
